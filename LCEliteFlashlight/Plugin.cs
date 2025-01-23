567a6f424d5710f98cc5ba98694041fdbf2f4fd1
MissileMann <misael.escoto043@gmail.com>
2025-01-23 00:13:59 -0600

    Add project files.

---
+++ LCEliteFlashlight/Plugin.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCEliteFlashlight
+{
+    [BepInPlugin(modGUID,modName, modVersion)]
+    [BepInDependency(LethalLib.Plugin.ModGUID)]
+    public class EliteFlashlightBase : BaseUnityPlugin
+    {
+        private const string modGUID = "Missile.LCEliteFlashlight";
+        private const string modName = "LCEliteFlashlight";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static EliteFlashlightBase Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("Elite Flashlight has awakened");
+
+            harmony.PatchAll();
+        }
+    }
+}
