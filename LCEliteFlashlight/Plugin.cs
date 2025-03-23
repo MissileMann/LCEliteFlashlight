6288dcf2cb16047a09ee6dc84b9769c1849e31e8
MissileMann <misael.escoto043@gmail.com>
2025-03-23 14:14:17 -0500

    added support for light utility for headlights

--- LCEliteFlashlight/Plugin.cs
+++ LCEliteFlashlight/Plugin.cs
@@ -6,17 +6,19 @@ using System.Reflection;
 using UnityEngine;
 using LethalLib.Modules;
 using LethalLib;
+using BepInEx.Bootstrap;
 
 namespace LCEliteFlashlight
 {
     [BepInDependency("FlipMods.ReservedItemSlotCore", BepInDependency.DependencyFlags.SoftDependency)]
+    [BepInDependency("hlb.lightutility", BepInDependency.DependencyFlags.SoftDependency)]
     [BepInPlugin(modGUID,modName, modVersion)]
     [BepInDependency(Plugin.ModGUID)]
     public class EliteFlashlightBase : BaseUnityPlugin
     {
         private const string modGUID = "Missile.LCEliteFlashlight";
         private const string modName = "LCEliteFlashlight";
-        private const string modVersion = "1.0.0";
+        private const string modVersion = "1.1.0";
 
         private readonly Harmony harmony = new Harmony(modGUID);
 
@@ -32,6 +34,9 @@ namespace LCEliteFlashlight
         void Awake()
         #pragma warning restore IDE0051 // Remove unused private members
         {
+            int elitePrice = 50;
+
+
             mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
             if (Instance == null)
             {
@@ -39,9 +44,9 @@ namespace LCEliteFlashlight
             }
             BoundConfig = new EliteFlashlightConfig(base.Config);
 
-            
+            //get asset from assetbundle
             string sAssemblyLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            
+
             EliteFlashlightAsset = AssetBundle.LoadFromFile(Path.Combine(sAssemblyLocation, "eliteflashlightbundle"));
             if (EliteFlashlightAsset == null )
             {
@@ -49,8 +54,10 @@ namespace LCEliteFlashlight
                 return;
             }
 
+            
+            //create the flashlight item
             Item Missile_EliteFlashlight = EliteFlashlightAsset.LoadAsset<Item>("assets/EliteFlashlightItem.asset");
-            if (Missile_EliteFlashlight == null )
+            if (Missile_EliteFlashlight == null)
             {
                 mls.LogError("Failed to load Elite-flashlight item");
                 return;
@@ -59,8 +66,8 @@ namespace LCEliteFlashlight
             NetworkPrefabs.RegisterNetworkPrefab(Missile_EliteFlashlight.spawnPrefab);
             Items.RegisterItem(Missile_EliteFlashlight);
 
-            int elitePrice = 50;
 
+            //set up shop item and price
             if(BoundConfig.EliteFlashlightPrice.Value > 0) { 
                 elitePrice = BoundConfig.EliteFlashlightPrice.Value;
             }
@@ -69,12 +76,18 @@ namespace LCEliteFlashlight
             TerminalNode eliteTerminalNode = EliteFlashlightAsset.LoadAsset<TerminalNode>("assets/iTerminalNode.asset");
             Items.RegisterShopItem(Missile_EliteFlashlight, null, null, eliteTerminalNode, elitePrice);
 
-            if (BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("FlipMods.ReservedFlashlightSlot"))
+            //compats
+            if (Chainloader.PluginInfos.ContainsKey("FlipMods.ReservedFlashlightSlot"))
             {
                 ReservedItemSlotsCompat.AddItemsToReservedItemSlots();
                 mls.LogInfo("Elite Flashlight and Flashlight slot compat loaded");
             }
             
+            if (Chainloader.PluginInfos.ContainsKey("hlb.lightutility"))
+            {
+                Missile_EliteFlashlight.spawnPrefab.GetComponent<FlashlightItem>().flashlightTypeID = 4;
+                mls.LogInfo("Elite Flashlight and light utility compat loaded");
+            }
 
             mls.LogInfo("Elite Flashlight has fully loaded");
             harmony.PatchAll();
